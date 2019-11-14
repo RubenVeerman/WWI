fbad94690630f3d97fb00469f47a98ea8ef39c4d
Glenn Nijenhuis <s1145509@student.windesheim.nl>
2019-11-14 11:57:14 +0100

    database functies toegevoegd

--- functions/databaseFunctions.php
+++ functions/databaseFunctions.php
@@ -26,3 +26,45 @@ function CreateConnection()
     return $conn; 
 }
 
+function SelecteerKlanten($connection)
+{
+    $sql = "SELECT nummer, naam, woonplaats FROM klant ORDER BY naam";
+    $result = mysqli_fetch_all(mysqli_query($connection, $sql), MYSQLI_ASSOC);
+    return $result;
+}
+
+function SluitVerbinding($connection) {
+    mysqli_close($connection);
+}
+
+function VoegKlantToe($connection, $naam, $woonplaats) {
+    $statement = mysqli_prepare($connection, "INSERT INTO klant (naam, woonplaats) VALUES(?,?)");
+    mysqli_stmt_bind_param($statement, 'ss', $naam, $woonplaats);
+    mysqli_stmt_execute($statement);
+    return mysqli_stmt_affected_rows($statement) == 1;
+}
+
+function ZitNaamInQueryString($string){
+    return isset($_GET[$string]);
+}
+
+function klantBestaat($naam)
+{
+    return mysqli_query(MaakVerbinding(), "select * from `klant` where naam=" .$naam);
+}
+
+function SelecteerKlant($connection, $nummer)
+{
+    $sql = "SELECT nummer, naam, woonplaats FROM klant WHERE nummer=" .$nummer;
+    $result = mysqli_fetch_all(mysqli_query($connection, $sql), MYSQLI_ASSOC);
+    return $result;
+}
+
+function BewerkKlant($connection, $nummer, $naam, $woonplaats) {
+    $statement = mysqli_prepare($connection, "UPDATE klant SET naam =?, woonplaats =? WHERE nummer =?");
+    mysqli_stmt_bind_param($statement, 'sss', $naam, $woonplaats, $nummer);
+    mysqli_stmt_execute($statement);
+    return mysqli_stmt_affected_rows($statement) == 1;
+}
+
+
