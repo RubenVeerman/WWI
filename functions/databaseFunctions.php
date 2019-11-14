91e400b6f80274aef82ff9e34e251683f962f726
Ronald <ronald@newheap.com>
2019-11-14 11:47:31 +0100

    start funcitons

--- functions/databaseFunctions.php
+++ functions/databaseFunctions.php
@@ -1,1 +1,28 @@
+<?php
+
+function CreateConnection() 
+{
+    //
+    // Create an connection with the given information from that ini file.
+    //
+    $conn = mysqli_connect("localhost", "root", "", "wideworldimporters");
+
+    //
+    // If there seems to be an error,
+    //
+    if (mysqli_connect_errno()) 
+    {
+        //
+        // Show the error and stop loading the website.
+        //
+        throw new Exception("Connection failed: " . mysqli_connect_error());
+    } 
+
+    if(!$conn)
+    {
+        throw new Exception("Something goes wrong mate!");
+    }
+
+    return $conn; 
+}
 
