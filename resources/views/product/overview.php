4906e4554d2b33b1c7dadcc5f239fc3b84286013
glennnijenhuis <glennnijenhuis@zoho.eu>
2019-11-29 11:48:25 +0100

    Images en overview verbeterd

--- resources/views/product/overview.php
+++ resources/views/product/overview.php
@@ -67,7 +67,7 @@ if($empty){
 for($i = 0; $i < count($products); $i++)
 {
     $product = $products[$i];
-    if($i % 3 == 0)
+    if($i % 4 == 0)
     {
         ?>
             </div>
@@ -76,16 +76,20 @@ for($i = 0; $i < count($products); $i++)
             <div class="row">
     <?php
     }
+
+    $arr = dbPhoto($product["StockItemID"]);
+//    echo "<pre>" . var_dump($arr) . "</pre>";
+//    die();
     ?>
 
-        <div class="col-sm-4">
+        <div class="col-sm-3">
             <a style="color: black" href="?page=product&action=show&id=<?= $product["StockItemID"] ?>">
                 <div class="card border-primary bg-light shadow" style="width: auto;">
-                    <img class="card-img-top" style="height: 150px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.mariescorner.com%2Fwp-content%2Fthemes%2Fmceighteen%2Fimg%2Fnopicture.png&f=1&nofb=1" alt="Card image cap">
+                    <img class="card-img-top img-fluid" style="height: 190px" src="<?=$arr[0]["Path"]?>" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title"><?= $product["StockItemName"]?></h5>
                         <h2 class="card-title">€ <?= $product["RecommendedRetailPrice"]?></h2>
-                        <a href="#" class="btn btn-success" style="width: 100%;">Voeg toe aan winkelwagen</a>
+                        <a href="#" class="btn btn-success" style="width: 100%;">Add to cart</a>
                     </div>
                 </div>
             </a>
