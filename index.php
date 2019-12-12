07f91d1c66264f501c719d9007a6aaab42b985eb
Ruben Veerman <ruben.veerman@windesheim.nl>
2019-12-12 12:08:45 +0100

    merge ahead

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once "./functions/core.php";
 require_once "./functions/authfunctions.php";
 require_once "./functions/databaseFunctions.php";
 startAuth();
-$_SESSION["Cart"] = [];
 prepareCart();
 
 // echo "<pre>";
